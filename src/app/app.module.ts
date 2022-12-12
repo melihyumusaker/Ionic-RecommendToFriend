28d838becf78fe465947656ea858f35709133e8b
melihyumusaker <74679907+melihyumusaker@users.noreply.github.com>
2022-12-13 01:23:56 +0300

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },SocialSharing],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx'
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private socialSharing: SocialSharing) {}
+
+  shareViaEmail(){
+    this.socialSharing.shareViaEmail('https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoidHIifQ%3D%3D%22%7D' , '' , ['melihyumusaker@gmail.com']).then(response => {
+      console.log(response); // Eğer başarılı bir şekilde cevap gelirse onu konsola bas
+    })
+    .catch(e => {
+      console.log(e); // Cevap gelmezse sorunu konsola bas
+    });
+  }
+
+ 
 
 }
